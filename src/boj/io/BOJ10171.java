2a9652fc348deffc3dc279de0e8e4a3fbecda004
Seong-Hwan <ksh970516@pukyong.ac.kr>
2020-01-23 19:16:00 +0900

    BOJ IO

---
+++ src/boj/io/BOJ10171.java
@@ -0,0 +1,10 @@
+package boj.io;
+
+public class BOJ10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
---
+++ src/boj/io/BOJ2557.java
@@ -0,0 +1,7 @@
+package boj.io;
+
+public class BOJ2557 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
