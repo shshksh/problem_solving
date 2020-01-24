75d9ebdd6b9233b48d17c92fa94c7ba98bb75a6c
Seong-Hwan <ksh970516@pukyong.ac.kr>
2020-01-24 17:59:19 +0900

    BOJ for

---
+++ src/boj/forphrase/BOJ2439.java
@@ -0,0 +1,19 @@
+package boj.forphrase;
+
+import java.util.Scanner;
+
+public class BOJ2439 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < n-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = n-i; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/boj/forphrase/BOJ2739.java
@@ -0,0 +1,13 @@
+package boj.forphrase;
+
+import java.util.Scanner;
+
+public class BOJ2739 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(n + " * " + i + " = " + n*i);
+        }
+    }
+}
