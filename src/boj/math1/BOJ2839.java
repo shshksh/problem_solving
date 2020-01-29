98e96a9bd488d7eeb4681913287dcbda23eab9fc
Seong-Hwan <ksh970516@pukyong.ac.kr>
2020-01-29 20:46:26 +0900

    BOJ math1

---
+++ src/boj/math1/BOJ2839.java
@@ -0,0 +1,22 @@
+package boj.math1;
+
+import java.util.Scanner;
+
+public class BOJ2839 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int weigh = sc.nextInt();
+        int sum = 0;
+        while(weigh > 0 &&  weigh%5 != 0) {
+            sum++;
+            weigh -= 3;
+        }
+        if(weigh < 0) {
+            if (weigh % 3 != 0)
+                System.out.println(-1);
+            else
+                System.out.println(sum);
+        } else
+            System.out.println(sum + weigh/5);
+    }
+}
