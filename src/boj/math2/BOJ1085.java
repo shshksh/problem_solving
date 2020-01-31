11e4d893c5c2a481bc3332b2c5c5ba8bb3f0da8e
Seong-Hwan <ksh970516@pukyong.ac.kr>
2020-01-31 23:13:55 +0900

    BOJ math2

---
+++ src/boj/math2/BOJ1085.java
@@ -0,0 +1,11 @@
+package boj.math2;
+
+import java.util.Scanner;
+
+public class BOJ1085 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt(), y = sc.nextInt(), w = sc.nextInt(), h = sc.nextInt();
+        System.out.println(Math.min(Math.min(x, w-x), Math.min(y, h-y)));
+    }
+}
