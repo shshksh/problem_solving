b49543c15ff4af7ffb0560b2220ca0a25488da3f
Seong-Hwan <ksh970516@pukyong.ac.kr>
2020-02-05 14:33:01 +0900

    programmers level1

---
+++ src/programmers/Level1/MySort.java
@@ -0,0 +1,14 @@
+package programmers.Level1;
+
+import java.util.Arrays;
+
+class MySort {
+    public String[] solution(String[] strings, int n) {
+        Arrays.sort(strings, (o1, o2) -> {
+            if(o1.charAt(n) == o2.charAt(n))
+                return o1.compareTo(o2);
+            return Character.compare(o1.charAt(n), o2.charAt(n));
+        });
+        return strings;
+    }
+}
---
+++ src/programmers/Level1/StrangeString.java
@@ -0,0 +1,16 @@
+package programmers.Level1;
+
+public class StrangeString {
+    public String solution(String s) {
+        char[] arr = s.toCharArray();
+        for (int i = 0, j = 0; i < arr.length; i++,j++) {
+            if(arr[i] == ' ')
+                j = 1;
+            if(j%2==0)
+                arr[i] = Character.toUpperCase(arr[i]);
+            else
+                arr[i] = Character.toLowerCase(arr[i]);
+        }
+        return new String(arr);
+    }
+}
