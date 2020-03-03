c76f7193ad5424251970b4e6092ccc0fa809b799
Seong-Hwan <ksh970516@pukyong.ac.kr>
2020-03-03 23:40:42 +0900

    programmers level2

---
+++ src/programmers/Level2/CoupleRemove.java
@@ -0,0 +1,17 @@
+package programmers.Level2;
+
+import java.util.Stack;
+
+public class CoupleRemove {
+    public int solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        stack.push(s.charAt(0));
+        for (int i = 1; i < s.length(); i++) {
+            if (!stack.empty() && s.charAt(i) == stack.peek())
+                stack.pop();
+            else
+                stack.push(s.charAt(i));
+        }
+        return stack.empty() ? 1 : 0;
+    }
+}
