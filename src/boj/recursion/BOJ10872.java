8a708bb27a6b52cdb62d77b4733793e117546fb3
Seong-Hwan <ksh970516@pukyong.ac.kr>
2020-03-09 21:26:10 +0900

    boj recursion

---
+++ src/boj/recursion/BOJ10872.java
@@ -0,0 +1,17 @@
+package boj.recursion;
+
+import java.util.Scanner;
+
+public class BOJ10872 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fact(n));
+    }
+
+    private static int fact(int n) {
+        if(n == 1 || n == 0)
+            return 1;
+        return n * fact(n-1);
+    }
+}
