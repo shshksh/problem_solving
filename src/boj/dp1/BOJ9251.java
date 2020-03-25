9c53aed31678be18e3f0c6a4680a18065416c6e0
Seong-Hwan <ksh970516@pukyong.ac.kr>
2020-03-25 22:12:06 +0900

    BOJ dp

---
+++ src/boj/dp1/BOJ9251.java
@@ -0,0 +1,26 @@
+package boj.dp1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ9251 {
+    static String a, b;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        a = br.readLine();
+        b = br.readLine();
+
+        int[][] arr = new int[a.length()+1][b.length()+1];
+
+        for (int i = 1; i <= a.length(); i++) {
+            for (int j = 1; j <= b.length(); j++) {
+                if (a.charAt(i-1) == b.charAt(j-1))
+                    arr[i][j] = arr[i - 1][j - 1] + 1;
+                else
+                    arr[i][j] = Math.max(arr[i - 1][j], arr[i][j - 1]);
+            }
+        }
+        System.out.println(arr[a.length()][b.length()]);
+    }
+}
