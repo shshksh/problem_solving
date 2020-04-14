14311c27d862a7f4959b12f4d1913ce3d7957569
SeongHwan <ksh970516@pukyong.ac.kr>
2020-04-15 01:38:10 +0900

    boj math3

---
+++ src/boj/math3/BOJ1676.java
@@ -0,0 +1,25 @@
+package boj.math3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ1676 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        int fact = 1, cnt = 0;
+        for (int i = 1; i <= t; i++) {
+            fact *= i;
+            while (fact % 10 == 0) {
+                fact /= 10;
+                cnt++;
+            }
+            while (fact > 100000)
+                fact -= fact/100000 * 100000;
+        }
+
+        System.out.println(cnt);
+    }
+}
