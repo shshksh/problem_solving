82f59caa4a20315dd8c42b7fb1598173f1855d39
SeongHwan <ksh970516@pukyong.ac.kr>
2020-04-23 19:02:43 +0900

    algorithm pa3, pa4

---
+++ src/algorithm_2020/pa4/Node.java
@@ -0,0 +1,17 @@
+package algorithm_2020.pa4;
+
+public class Node {
+    Node leftNode;
+    Node rightNode;
+    Node superNode;
+    CSVData data;
+
+    public Node(String[] info) {
+        data = new CSVData(info);
+    }
+
+    @Override
+    public String toString() {
+        return data.toString();
+    }
+}
