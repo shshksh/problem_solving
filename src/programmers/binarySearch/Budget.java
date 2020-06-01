f69e052f3f48b5c7a6021c46509c056af14fc0ca
Seong-Hwan <ksh970516@pukyong.ac.kr>
2020-06-01 18:48:17 +0900

    programmers binary search

--- src/programmers/binarySearch/Budget.java
+++ src/programmers/binarySearch/Budget.java
@@ -1,33 +1,29 @@
 package programmers.binarySearch;
 
-import java.util.Arrays;
-
 public class Budget {
-    static int max;
-
-    public static int solution(int[] budgets, int M) {
-        int length = budgets.length;
-        Arrays.sort(budgets);
-        max = M/length;
-
+    public int solution(int[] budgets, int M) {
+        int lb = 0, ub = budgets[0], mid;
+        long sum = 0;
         for (int budget : budgets) {
-            if(budget < max) {
-                M-=budget;
-                length--;
-            } else {
-                max = M/length;
-                if(budget < max) {
-                    M-=budget;
-                    length--;
-                } else
-                    break;
-            }
+            ub = Math.max(ub, budget);
+            sum += budget;
         }
-
-        return length==0?M:max;
+        if (sum < M)
+            return ub;
+        return binaryBudgets(budgets, M, lb, ub);
     }
 
-    public static void main(String[] args) {
-        System.out.println(solution(new int[] {1,2,3,4}, 485));
+    private int binaryBudgets(int[] budgets, int M, int lb, int ub) {
+        if (lb > ub)
+            return ub;
+        long sum = 0;
+        int mid  = (lb+ub)/2;
+        for (int budget : budgets) {
+            sum += Math.min(budget, mid);
+        }
+        if (sum >= M)
+            return binaryBudgets(budgets, M, lb, mid-1);
+        else
+            return binaryBudgets(budgets, M, mid+1, ub);
     }
 }
