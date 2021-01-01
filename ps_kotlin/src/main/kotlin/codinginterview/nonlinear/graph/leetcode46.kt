24f6bf06869ef6c39d82531f98f449dcf20d539d
Seonghwan Kim <ksh970516@pukyong.ac.kr>
2021-01-01 23:52:48 +0900

    algorithm210101

---
+++ ps_kotlin/src/main/kotlin/codinginterview/nonlinear/graph/leetcode46.kt
@@ -0,0 +1,23 @@
+package codinginterview.nonlinear.graph
+
+fun permute(nums: IntArray): List<List<Int>> {
+    val exclude = mutableListOf<Int>()
+    val result = mutableListOf<List<Int>>()
+
+    fun permuteOf(include: MutableList<Int>, exclude: MutableList<Int>) {
+        if (include.isEmpty()) {
+            result.add(exclude)
+        }
+        for (e in include) {
+            permuteOf((include - e) as MutableList<Int>, (exclude + e) as MutableList<Int>)
+        }
+    }
+
+    permuteOf(nums.toMutableList(), exclude)
+
+    return result
+}
+
+fun main() {
+    println(permute(intArrayOf(1, 2, 3)))
+}
