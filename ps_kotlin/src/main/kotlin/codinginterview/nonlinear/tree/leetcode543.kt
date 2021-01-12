6ef0b6dbd3e2d167778d1c8a671032090b922efa
Seonghwan Kim <ksh970516@pukyong.ac.kr>
2021-01-12 20:52:42 +0900

    algoithm210112

---
+++ ps_kotlin/src/main/kotlin/codinginterview/nonlinear/tree/leetcode543.kt
@@ -0,0 +1,20 @@
+package codinginterview.nonlinear.tree
+
+import java.lang.Integer.max
+
+fun diameterOfBinaryTree(root: TreeNode?): Int {
+
+    var diameter = 0
+
+    fun dfs(root: TreeNode?): Int {
+        if (root == null)
+            return 0;
+        val left = dfs(root.left)
+        val right = dfs(root.right)
+        diameter = max(diameter, left + right)
+        return max(left, right) + 1
+    }
+    dfs(root)
+
+    return diameter
+}
