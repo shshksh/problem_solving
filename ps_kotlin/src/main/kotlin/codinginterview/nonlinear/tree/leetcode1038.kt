b46d3c83a1e13212856cc66bf7d024623802b25a
Seonghwan Kim <ksh970516@pukyong.ac.kr>
2021-01-28 14:26:17 +0900

    algorithm210127

---
+++ ps_kotlin/src/main/kotlin/codinginterview/nonlinear/tree/leetcode1038.kt
@@ -0,0 +1,18 @@
+package codinginterview.nonlinear.tree
+
+fun bstToGst(root: TreeNode?): TreeNode? {
+    var acc = 0
+
+    fun dfs(node: TreeNode?) {
+        node ?: return
+
+        dfs(node.right)
+        node.`val` += acc
+        acc = node.`val`
+        dfs(node.left)
+    }
+
+    dfs(root)
+
+    return root
+}
