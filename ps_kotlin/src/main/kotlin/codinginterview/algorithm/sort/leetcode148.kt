c9023c10a6c290c1e01bfd8a2fb16bc3b180966a
Seonghwan Kim <ksh970516@pukyong.ac.kr>
2021-02-13 21:14:26 +0900

    algorithm210213

---
+++ ps_kotlin/src/main/kotlin/codinginterview/algorithm/sort/leetcode148.kt
@@ -0,0 +1,26 @@
+package codinginterview.algorithm.sort
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
+fun sortList(head: ListNode?): ListNode? {
+    val arrayList = mutableListOf<Int>()
+
+    var curr = head
+    while (curr != null) {
+        arrayList.add(curr.`val`)
+        curr = curr.next
+    }
+
+    arrayList.sortDescending()
+    var newHead: ListNode? = null
+
+    for (`val` in arrayList) {
+        val tmp = ListNode(`val`)
+        tmp.next = newHead
+        newHead = tmp
+    }
+
+    return newHead
+}
