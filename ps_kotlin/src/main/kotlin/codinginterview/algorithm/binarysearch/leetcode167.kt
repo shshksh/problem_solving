a9b05509a97dfffbae4fefee74a6ac75dbbce06d
Seonghwan Kim <ksh970516@pukyong.ac.kr>
2021-02-25 14:53:49 +0900

    algorithm210225

---
+++ ps_kotlin/src/main/kotlin/codinginterview/algorithm/binarysearch/leetcode167.kt
@@ -0,0 +1,15 @@
+package codinginterview.algorithm.binarysearch
+
+fun twoSum(numbers: IntArray, target: Int): IntArray {
+    var i = 0
+    var j = numbers.size - 1
+
+    while (i < j) {
+        when {
+            numbers[i] + numbers[j] < target -> i++
+            numbers[i] + numbers[j] > target -> j--
+            else -> return intArrayOf(i, j)
+        }
+    }
+    return intArrayOf()
+}
