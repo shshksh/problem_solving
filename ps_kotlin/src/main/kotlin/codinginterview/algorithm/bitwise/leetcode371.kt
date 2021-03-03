6bba252d7b3523f8ccbf410c9d87d13bd52f22a2
Seonghwan Kim <ksh970516@pukyong.ac.kr>
2021-03-03 12:49:55 +0900

    algorithm210303

---
+++ ps_kotlin/src/main/kotlin/codinginterview/algorithm/bitwise/leetcode371.kt
@@ -0,0 +1,22 @@
+package codinginterview.algorithm.bitwise
+
+fun getSum(a: Int, b: Int): Int {
+    var carry = false
+    var (bit1, bit2) = false to false
+
+    var mask = 1
+    var result = 0
+
+    for (i in 0 until 32) {
+        bit1 = a and mask != 0
+        bit2 = b and mask != 0
+
+        if (bit1 xor bit2 xor carry)
+            result = result or mask
+
+        carry = (bit1 and bit2) or (bit1 and carry) or (bit2 and carry)
+        mask = mask shl 1
+    }
+
+    return result
+}
