bdaf280bc9cd175a8dab930039a6d6ea4e4d79cc
SeongHwan Kim <shshksh58@gmail.com>
2021-06-17 13:42:32 +0900

    algorithm 210616 백트래킹 1

---
+++ ps_kotlin/src/main/kotlin/boj/boj9095.kt
@@ -0,0 +1,26 @@
+package boj
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val bw = System.out.bufferedWriter()
+
+    val t = br.readLine().toInt()
+    var result = 0
+
+    fun dfs(n: Int, acc: Int) {
+        if (acc == n)
+            result++
+        if (acc > n)
+            return
+        dfs(n, acc + 1)
+        dfs(n, acc + 2)
+        dfs(n, acc + 3)
+    }
+
+    repeat(t) {
+        result = 0
+        dfs(br.readLine().toInt(), 0)
+        bw.write("$result\n")
+    }
+    bw.flush()
+}
