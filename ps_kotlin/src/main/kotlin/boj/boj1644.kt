3bca9cc757ad6a34044b4d9c84cbd6aef9877b0f
SeongHwan Kim <shshksh58@gmail.com>
2021-06-22 18:50:48 +0900

    algorithm 210622 기초 수학 2

---
+++ ps_kotlin/src/main/kotlin/boj/boj1644.kt
@@ -0,0 +1,44 @@
+package boj
+
+import kotlin.math.sqrt
+
+fun main() {
+    val n = readLine()!!.toInt()
+    val flag = BooleanArray(n + 1) { true }
+    flag[1] = false
+
+    for (i in 2..sqrt(n.toDouble()).toInt()) {
+        if (i >= 2) {
+            var tmp = i + i
+            while (tmp <= n) {
+                flag[tmp] = false
+                tmp += i
+            }
+        }
+    }
+
+    val primes = mutableListOf<Int>()
+    for (i in 2 until flag.size) {
+        if (flag[i])
+            primes += i
+    }
+
+    var count = 0
+    for (i in primes.indices) {
+        var sum = 0
+        if (primes[i] > n / 2)
+            break
+        for (j in i until primes.size) {
+            sum += primes[j]
+            if (sum == n) {
+                count++
+            } else if (sum > n) {
+                break
+            }
+        }
+    }
+    if (n in primes)
+        count++
+
+    println(count)
+}
