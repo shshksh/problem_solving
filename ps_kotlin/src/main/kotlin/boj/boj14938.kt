f24b166fec3cc6e85efe461ef1c727e51fafc06a
Seonghwan Kim <shshksh58@gmail.com>
2021-12-07 21:39:31 +0900

    algorithm 211207

---
+++ ps_kotlin/src/main/kotlin/boj/boj14938.kt
@@ -0,0 +1,36 @@
+package boj
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+
+    val (nodeCount, searchLimit, edgeCount) = br.readLine().split(" ").map { it.toInt() }
+
+    val itemList = br.readLine().split(" ").map { it.toInt() }
+
+    val weightArray = Array(nodeCount) { IntArray(nodeCount) { 10000000 } }
+
+    for (edge in 1..edgeCount) {
+        val (from, to, weight) = br.readLine().split(" ").map { it.toInt() }
+        weightArray[from - 1][to - 1] = weight
+        weightArray[to - 1][from - 1] = weight
+    }
+
+    for (k in 0 until nodeCount)
+        for (i in 0 until nodeCount)
+            for (j in 0 until nodeCount) {
+                if (i == j) continue
+                weightArray[i][j] = minOf(weightArray[i][j], weightArray[i][k] + weightArray[k][j])
+            }
+
+    var max = 0
+    for (i in 0 until nodeCount) {
+        var sum = itemList[i]
+        for ((j, weight) in weightArray[i].withIndex()) {
+            if (weight <= searchLimit)
+                sum += itemList[j]
+        }
+        max = maxOf(max, sum)
+    }
+
+    println(max)
+}
